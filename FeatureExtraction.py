423d8701eb7f5da39e6c765c2192a302e349949f
PraiseX <peteng@wpi.edu>
2021-11-09 13:53:48 -0500

    added lsf and lpc features kind of

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -1,5 +1,9 @@
 from python_speech_features import mfcc
 from python_speech_features import logfbank
+import math
+import numpy as np
+from scikits.talkbox.linpred.levinson_lpc import levinson, acorr_lpc, lpc
+
 # import scipy.io.wavfile as wav
 
 # (rate,sig) = wav.read("file.wav")
@@ -7,3 +11,29 @@ mfcc_feat = mfcc(sig,rate)
 fbank_feat = logfbank(sig,rate)
 
 print(fbank_feat[1:3,:])
+
+
+#lsf implementation https://pyspectrum.readthedocs.io/en/latest/install.html
+
+def lpc(y, m):
+    "Return m linear predictive coefficients for sequence y using Levinson-Durbin prediction algorithm"
+    #step 1: compute autoregression coefficients R_0, ..., R_m
+    R = [y.dot(y)] 
+    if R[0] == 0:
+        return [1] + [0] * (m-2) + [-1]
+    else:
+        for i in range(1, m + 1):
+            r = y[i:].dot(y[:-i])
+            R.append(r)
+        R = np.array(R)
+    #step 2: 
+        A = np.array([1, -R[1] / R[0]])
+        E = R[0] + R[1] * A[1]
+        for k in range(1, m):
+            if (E == 0):
+                E = 10e-17
+            alpha = - A[:k+1].dot(R[k+1:0:-1]) / E
+            A = np.hstack([A,0])
+            A = A + alpha * A[::-1]
+            E *= (1 - alpha**2)
+        return A
