7f0404c8ee0068634d864ac8fc3014f916a5b498
PraiseX <peteng@wpi.edu>
2021-11-30 01:11:21 -0500

    got the avg and std

--- classifier.py
+++ classifier.py
@@ -7,6 +7,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import MinMaxScaler, KBinsDiscretizer
 from sklearn.model_selection import cross_val_score, GridSearchCV, KFold, RandomizedSearchCV, train_test_split
 from sklearn import svm
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
 
 
 import xgboost as xgb                         
@@ -41,6 +42,7 @@ train_user_id = train_list['Participant_ID']
 train_PHQ8B = train_list['PHQ8_Score'] 
 
 est = KBinsDiscretizer(n_bins=3, encode='ordinal', strategy='uniform')
+est2 = KBinsDiscretizer(n_bins=2, encode='ordinal', strategy='uniform')
 
 
 test_list = pd.read_csv('D:\MQP\dataset\DAICWOZ\dev_split_Depression_AVEC2017.csv')                                                         
@@ -55,16 +57,25 @@ au_file_test = []
 for train_u in train_user_id: 
     #depending on the file use different seperators
     au_file_tmp = pd.read_csv(path+'{}_CLNF_AUs.txt'.format(train_u), sep = ', ') 
+    #print(type(au_file_tmp))
+    au_file_tmp_mean = au_file_tmp.mean(axis=0)
+    au_file_tmp_std = au_file_tmp.std(axis=0) # calculate the std of each column
+    # print("mean", au_file_tmp_mean)
+    # print("std", au_file_tmp_std)
+    concate  = pd.concat([au_file_tmp_mean, au_file_tmp_std], axis=1)
+    au_file.append(concate)
     au_file.append(au_file_tmp[['confidence', 'success', 'AU01_r', 'AU02_r', 'AU04_r', 'AU05_r', 'AU06_r', 'AU09_r', 'AU10_r', 'AU12_r', 'AU14_r', 'AU15_r', 'AU17_r', 'AU20_r', 'AU25_r', 'AU26_r', 'AU04_c', 'AU12_c', 'AU15_c', 'AU23_c', 'AU28_c', 'AU45_c']])  
-    label = np.array(['confidence', 'success', 'AU01_r', 'AU02_r', 'AU04_r', 'AU05_r', 'AU06_r', 'AU09_r', 'AU10_r', 'AU12_r', 'AU14_r', 'AU15_r', 'AU17_r', 'AU20_r', 'AU25_r', 'AU26_r', 'AU04_c', 'AU12_c', 'AU15_c', 'AU23_c', 'AU28_c', 'AU45_c'])
-    cols=pd.MultiIndex.from_product(label)
-    au_reshaped = np.reshape(au_file,(29565, 22))
-    au_frame = pd.DataFrame(au_reshaped)
-    print("au_file:" , au_frame.mean(axis=1))
-
+                                                               
+    
 for test_u in test_user_id: 
     #depending on the file use different seperators
     au_file_test_tmp = pd.read_csv(path+'{}_CLNF_AUs.txt'.format(train_u), sep = ', ') 
+    au_file_test_tmp_mean = au_file_test_tmp.mean(axis=0)
+    au_file_test_tmp_std = au_file_test_tmp.std(axis=0) # calculate the std of each column
+    # print("mean", au_file_tmp_mean)
+    # print("std", au_file_tmp_std)
+    concate  = pd.concat([au_file_test_tmp_mean, au_file_test_tmp_std], axis=1)
+    au_file_test.append(concate)
     au_file_test.append(au_file_test_tmp[['confidence', 'success', 'AU01_r', 'AU02_r', 'AU04_r', 'AU05_r', 'AU06_r', 'AU09_r', 'AU10_r', 'AU12_r', 'AU14_r', 'AU15_r', 'AU17_r', 'AU20_r', 'AU25_r', 'AU26_r', 'AU04_c', 'AU12_c', 'AU15_c', 'AU23_c', 'AU28_c', 'AU45_c']]) 
     
     # print("train_u:" , train_u) 
@@ -72,12 +83,19 @@ for test_u in test_user_id:
 
 # avg_score = train_PHQ8B.mean / train_PHQ8B.__len__ - 1                      
 
+print("mean:", au_file_tmp_mean)
+print("std:", au_file_tmp_std)
+
 au_file = pd.concat(au_file, axis=0)    
+au_file_test = pd.concat(au_file_test, axis=0)    
+
 
 
 scaler = MinMaxScaler()                                                
 
 au_file = scaler.fit_transform(au_file)  
+au_file_test = scaler.fit_transform(au_file_test) 
+
 
 regr = RandomForestRegressor(max_depth=2, random_state=0)              
 clf=svm.SVC()
@@ -85,34 +103,34 @@ xgb_model = xgb.XGBRegressor(objective="reg:linear", random_state=42)
 
 # groundtruth = train_list['PHQ8_Score'] 
                     
-
-au_file_test = scaler.transform(au_file_test) 
-
-
-
-truerger = regr.fit(au_file, train_PHQ8B) 
-clf.fit(au_file, train_PHQ8B)
-xgb_model.fit(au_file, train_PHQ8B)
-
+trainregr = regr.fit(au_file, train_PHQ8B) 
+trainclf = clf.fit(au_file, train_PHQ8B)
+trainxgb = xgb_model.fit(au_file, train_PHQ8B)
+#include naive bayes
 
 predictionRegr = regr.predict(au_file_test)   
 predictionSVM = clf.predict(au_file_test)                             
 predictionXGB = xgb_model.predict(au_file_test)
 
 est.fit(train_PHQ8B)      
+est2.fit(train_PHQ8B)
+
 
 threebins = est.transform(predictionRegr)
-threebinsgroundtruth = est.transform(test_PHQ8B)
+threebinsgt = est.transform(test_PHQ8B)
+
+twobins = est2.transform(predictionRegr)
+twobinsgt = est2.transform(test_PHQ8B)
 
 #average all predictions
 #
 
 regrROC = sklearn.metrics.roc_auc_score(test_PHQ8B, clf.predict_proba(au_file_test))
+accuracy = accuracy_score(test_PHQ8B, au_file_test)
+f1 = f1_score(test_PHQ8B, au_file_test)
+tn, fp, fn, tp = confusion_matrix(test_PHQ8B, au_file_test).ravel()
 
-
-
-
- 
+#print("auc_roc:", regrROC)
 
 
 
