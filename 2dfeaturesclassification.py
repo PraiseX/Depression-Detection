12abfc389b5281718547d7cebbc07e17743bbd09
PraiseX <peteng@wpi.edu>
2022-01-17 12:00:53 -0500

    Finished Action Units, still need to procure results before moving onto 2d features which is not done yet. I should refactor my code but out of interest of time I am not worried about that right now.

--- 2dfeaturesclassification.py
+++ 2dfeaturesclassification.py
@@ -34,7 +34,7 @@ import joblib
 
 
 #replace with appropriate path
-path = "D:\MQP\dataset\DAICWOZ\Facial Features\Action Units\\"
+path = "D:\MQP\dataset\DAICWOZ\Facial Features\2D Features\\"
 filenames = glob.glob(path + "\*.csv")
 
 #replace with appropriate path
@@ -97,7 +97,7 @@ print("---loading AU Files----")
 #make sure all the participants from the training split are in folder
 for idx, train_u in enumerate(train_user_id):
     #depending on the file use different seperators
-    au_file_tmp = pd.read_csv(path+'{}_CLNF_AUs.txt'.format(train_u), sep = ', ')
+    au_file_tmp = pd.read_csv(path+'{}_CLNF_features.txt'.format(train_u), sep = ', ')
     #au_file_tmp_mean = au_file_tmp.iloc[:, -22:].mean(axis=0)
     #au_file_tmp_std = au_file_tmp.iloc[:, -22:].std(axis=0)
     #print(type(au_file_tmp))
@@ -114,7 +114,8 @@ for idx, train_u in enumerate(train_user_id):
     #concate  = pd.concat([au_file_tmp_mean, au_file_tmp_std], axis=0)
     #au_file.append(concate)
     au_file.append(au_file_tmp[['confidence', 'success', 'AU01_r', 'AU02_r', 'AU04_r', 'AU05_r', 'AU06_r', 'AU09_r', 'AU10_r', 'AU12_r', 'AU14_r', 'AU15_r', 'AU17_r', 'AU20_r', 'AU25_r', 'AU26_r', 'AU04_c', 'AU12_c', 'AU15_c', 'AU23_c', 'AU28_c', 'AU45_c']])  
-                                                               
+    #frame, timestamp, confidence, success, x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24, x25, x26, x27, x28, x29, x30, x31, x32, x33, x34, x35, x36, x37, x38, x39, x40, x41, x42, x43, x44, x45, x46, x47, x48, x49, x50, x51, x52, x53, x54, x55, x56, x57, x58, x59, x60, x61, x62, x63, x64, x65, x66, x67, y0, y1, y2, y3, y4, y5, y6, y7, y8, y9, y10, y11, y12, y13, y14, y15, y16, y17, y18, y19, y20, y21, y22, y23, y24, y25, y26, y27, y28, y29, y30, y31, y32, y33, y34, y35, y36, y37, y38, y39, y40, y41, y42, y43, y44, y45, y46, y47, y48, y49, y50, y51, y52, y53, y54, y55, y56, y57, y58, y59, y60, y61, y62, y63, y64, y65, y66, y67
+                                                            
 
 
 for idx, test_u in enumerate(test_user_id): 
